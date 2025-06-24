723a18f23d16d0b3f266055ba68e500a8fd2f2b6
Trieu <nguyenngoctram762@gmail.com>
2025-06-24 17:37:49 +0700

    add commit to pull

--- src/main/java/com/unisew/profile_service/controllers/ProfileController.java
+++ src/main/java/com/unisew/profile_service/controllers/ProfileController.java
@@ -5,6 +5,7 @@ import com.unisew.profile_service.services.ProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +25,5 @@ public class ProfileController {
     public ResponseEntity<ResponseObject> getAllGarmentProfile() {
         return profileService.getAllGarmentProfile();
     }
+
 }
